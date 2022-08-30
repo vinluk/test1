a6fcb4863f88c1f2c58eddce1f0f8d2904400877
vinluk9@gmail.com <vinluk95@gmail.com>
2022-08-30 17:39:24 +0800

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
  
-app.listen(port, () =>{
+app.listen(process.env.PORT || port, () =>{
     console.log("Server is running on port "+port+"...");
 });
  
